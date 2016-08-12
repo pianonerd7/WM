f063159ad17bcf706ed354aa5d4cde3885fdcb83
Anna He <anna.he@uber.com>
2016-08-11 23:04:51 -0700

    gets a synset slice

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"syntacticsub/usubstitute"
 	"syntacticsub/wordnet"
 )
 
 func main() {
 	wordnet.InitWN()
-	word := "laughter"
-	fmt.Printf("Sense Results for '%s': %s\n", word, wordnet.FindTheInfo(word, 1, 5, 0))
-	fmt.Printf("Sense Results for '%s': %s\n", word, wordnet.FindTheInfo_ds(word, 1, 5, 0))
+	word := "happy"
+	//fmt.Printf("Sense Results for '%s': %s\n", word, wordnet.FindTheInfo(word, 1, 5, 0))
+	//fmt.Printf("Sense Results for '%s': %s\n", word, wordnet.FindTheInfo_ds(word, 1, 5, 0))
+	fmt.Println(usubstitute.GetSynset(word))
 }
--- usubstitute/usubstitute.go
+++ usubstitute/usubstitute.go
@@ -1,1 +1,16 @@
 package usubstitute
+
+import (
+	"strings"
+
+	"syntacticsub/wordnet"
+)
+
+func GetSynset(word string) []string {
+	resultString := wordnet.FindTheInfo_ds(word, 1, 5, 0)
+
+	trimLeftCurly := strings.Trim(resultString, "{")
+	trimRightCurly := strings.Trim(trimLeftCurly, "}")
+
+	return strings.Split(trimRightCurly, ",")
+}
