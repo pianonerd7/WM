2ea0f2d33ff510499d3d1c683cfa2991f95d812a
Anna He <anna.he@uber.com>
2016-08-16 16:54:02 -0700

    extracts secret

--- watermark/watermark.go
+++ watermark/watermark.go
@@ -22,3 +22,39 @@ func EmbedMessage(message string) string {
 
 	return watermarkedMessage.String()
 }
+
+func ExtractMessage(originalMessage, embeddedMessage string) string {
+	synsetMap := usubstitute.GetMapFromMessage(originalMessage)
+	wordWithPuncOriginal := usubstitute.MessageToWords(originalMessage)
+	wordWithPuncEmbedded := usubstitute.MessageToWords(embeddedMessage)
+
+	var watermarkedMessage bytes.Buffer
+
+	for index, word := range wordWithPuncOriginal {
+		if synsetMap[word] != nil {
+			if word != wordWithPuncEmbedded[index] {
+				watermarkedMessage.WriteString("1")
+			} else {
+				watermarkedMessage.WriteString("0")
+			}
+		}
+	}
+	return watermarkedMessage.String()
+}
+
+func EmbedFullMessage(message string) string {
+	synsetMap := usubstitute.GetMapFromMessage(message)
+	wordWithPunc := usubstitute.MessageToWords(message)
+
+	var watermarkedMessage bytes.Buffer
+
+	for _, word := range wordWithPunc {
+		if synsetMap[word] != nil {
+			watermarkedMessage.WriteString(synsetMap[word][0] + " ")
+		} else {
+			watermarkedMessage.WriteString(word + " ")
+		}
+	}
+
+	return watermarkedMessage.String()
+}
