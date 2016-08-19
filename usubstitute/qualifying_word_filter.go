d67ffd8eeec277c0bd6513b72ef8588290c8ab7b
Anna He <anna.he@uber.com>
2016-08-18 19:39:24 -0700

    added a function to check for original message words

--- usubstitute/qualifying_word_filter.go
+++ usubstitute/qualifying_word_filter.go
@@ -4,11 +4,15 @@ import (
 	"strings"
 )
 
-func didPassAllFilter(newSense string, uniqueSenses []string) bool {
+func didPassAllFilterForSynset(newSense string, uniqueSenses []string) bool {
 	if !isStringInSlice(newSense, uniqueSenses) && isOneWord(newSense) {
 		return false
 	}
 	return true
+}
+
+func isQualifyingWord(word string) bool {
+	return isOneWord(word)
 }
 
 func isStringInSlice(word string, list []string) bool {
@@ -21,7 +25,7 @@ func isStringInSlice(word string, list []string) bool {
 }
 
 func isOneWord(word string) bool {
-	if strings.ContainsAny(word, "_ ( ) 1 2 3 4 5 6 7 8 9 0 '") {
+	if strings.ContainsAny(word, "_ ( ) 1 2 3 4 5 6 7 8 9 0 ' ") {
 		return false
 	}
 	return true
