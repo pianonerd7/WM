ce1f2cc06dd383051f9245b63f10dd1b531b20e8
Anna He <anna.he@uber.com>
2016-08-22 15:55:24 -0700

    added cases

--- main.go
+++ main.go
@@ -1,33 +1,30 @@
 package main
 
-import (
-	"fmt"
-
-	"code.uber.internal/engsec/syntacticsub/watermark"
-	"code.uber.internal/engsec/syntacticsub/wordnet"
-)
+import "code.uber.internal/engsec/syntacticsub/wordnet"
 
 func main() {
 	wordnet.InitWN()
 	//010100101111110101010010111111010101001011111101 should be
-	//010100101111110101010010111111010101       actual
+	//0101001011111101010100            actual
 	//[0 1 0 1 0 0 1 0 1 1 1 1 1 1 0 1]
-	word := "It was a big, bold idea, especially given that Uber was still a relatively small and no one in China had ever even heard of us.  And of course, anytime we got into a discussion about our efforts in China, most people thought we were naive, crazy – or both. We saw things differently of course. China is an inspiring country with astonishing opportunity. Many of the world’s mega cities are Chinese, and their thirst for transportation innovation is second to none. Uber’s mission to make “transportation as reliable as running water, everywhere for everyone” resonates especially strongly in China."
-	embeded := "It was a big , bluff idea , particularly given that Uber was still a relatively little and no one in China had always yet heard of us . And of class , anytime we got into a treatment about our efforts in PRC , most citizenry thought we were naive , brainsick or both . We saw things differently of class . China is an inspiring state with astonishing opportunity . many of the world’s mega cities are Chinese , and their thirstiness for transit invention is second to none . Uber’s charge to do “ transit as reliable as running urine , everywhere for everyone ” resonates particularly strongly in PRC ."
-	//fmt.Println(wordnet.GetSenseLength("happily", 3, 23))
-	//fmt.Println(wordnet.GetSenseLength("blue", 3, 23))
-	//fmt.Printf("Sense Results for '%s': %s\n", "blue", wordnet.FindTheInfo_ds("blue", 3, 23, 0))
-	//fmt.Printf("Sense Results for '%s': %s\n", "blue", wordnet.FindTheInfo("blue", 3, 2, 0))
-	//fmt.Printf("Sense Results for '%s': %s\n", "blue", wordnet.FindTheInfo("blue", 3, 3, 0))
-
-	//fmt.Printf("Sense Results for '%s': %s\n", "blue", wordnet.FindTheInfo_ds("blue", 1, 5, 0))
-	/*x := usubstitute.GetMapFromMessage(word)
-	for key, value := range x {
-		fmt.Printf("%v , %v \n", key, value)
-	}*/
+	//[0 1 0 1 0 0 1 0 1 1 1 1 1 1 0 1]
+	//word := "Being an entrepreneur means you are an explorer by nature, doing what everyone thinks is impossible but with an optimistic perspective on the unknown. Uber entered this uncharted territory in February 2014, two years after Didi was founded. We were a young American business entering a country where most US internet companies had failed to crack the code, and with a product that needed rebuilding. Our China effort has been one of Uber's most entrepreneurial because we literally had to start from scratch."
+	//embeded := "Being an entrepreneur means you are an adventurer by nature, doing what everyone thinks is impossible but with an affirmative perspective on the unknown. Uber entered this chartless territory in Feb, ii age after Didi was founded. We were a immature American concern entering a state where most US internet companies had failed to cleft the code, and with a merchandise that needed rebuilding. Our China attempt has been one of Uber's most entrepreneurial because we literally had to start from scratch."
 	//fmt.Println(watermark.EmbedMessage(word))
-	fmt.Println(watermark.ExtractMessage(word, embeded))
+	//fmt.Println(watermark.ExtractMessage(word, embeded))
 	//watermark.EmbedMessage(word)
 	//watermark.ExtractMessage(word, embeded)
-	//usubstitute.GetMapFromMessage(word)
+
+	/*
+		args := os.Args[1:]
+		text := args[0]
+		command, _ := strconv.Atoi(args[1])
+		switch command {
+		case 1:
+			fmt.Printf(watermark.EmbedFullMessage(text))
+		case 2:
+			fmt.Printf("hello world")
+		}
+	*/
+
 }
