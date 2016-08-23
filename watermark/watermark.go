0acd373b740fc4de70268b6daa6f1e685e056ec9
Anna He <anna.he@uber.com>
2016-08-22 19:16:37 -0700

    function that takes a slice of email and embeds them all

--- watermark/watermark.go
+++ watermark/watermark.go
@@ -4,16 +4,27 @@ import (
 	"bytes"
 	"fmt"
 
+	"code.uber.internal/engsec/syntacticsub/sql"
 	"code.uber.internal/engsec/syntacticsub/usubstitute"
 	"code.uber.internal/engsec/syntacticsub/utility"
 )
 
+func EmbedMessageForAllEmail(message string, emails []string) []string {
+
+	var embeddedMessages []string
+	for _, email := range emails {
+		bitSecret := utility.GetWaterMark(email)
+		fmt.Println(bitSecret)
+		embeddedMessages = append(embeddedMessages, EmbedMessage(message, bitSecret))
+	}
+
+	return embeddedMessages
+}
+
 // EmbedMessage takes a message, generates a secret and watermarks the message using the secret
 // and returns the message with the embedded message
-func EmbedMessage(message string) string {
+func EmbedMessage(message string, bitSecret []utility.Bit) string {
 	synsetMap := usubstitute.GetMapFromMessage(message)
-	bitSecret := utility.GetRandomBytes()
-	fmt.Println(bitSecret)
 	wordWithPunc := usubstitute.MessageToWords(message)
 
 	bitIndex := 0
@@ -71,7 +82,9 @@ func ExtractMessage(originalMessage, embeddedMessage string) string {
 			}
 		}
 	}
-	return watermarkedMessage.String()
+	//return watermarkedMessage.String()
+	watermark := watermarkedMessage.String()
+	return sql.GetUserEmailFromWaterMark(watermark[:16])
 }
 
 // EmbedFullMessage takes a message and embeds the message with all '1'
