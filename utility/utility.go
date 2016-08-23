256a00aa7cb5f8c467b10c2a30023597e9eba4d0
Anna He <anna.he@uber.com>
2016-08-22 19:16:05 -0700

    get random bytes and also checks to see if there exists a watermark for a user, if not, then add

--- utility/utility.go
+++ utility/utility.go
@@ -3,6 +3,8 @@ package utility
 import (
 	"math/rand"
 	"time"
+
+	"code.uber.internal/engsec/syntacticsub/sql"
 )
 
 func Random(min, max int) int {
@@ -11,9 +13,35 @@ func Random(min, max int) int {
 }
 
 // getRandomBytes return a splice of bits
-func GetRandomBytes() []Bit {
+func GetRandomBytes() ([]Bit, string) {
+	var bits []Bit
+	var stringBits string
+	count := -1
+	for count != 0 {
+		bits = getRandomByte()
+		stringBits = ToString(bits)
+		count = sql.CountUserWatermark(stringBits)
+	}
+	return bits, stringBits
+}
+
+func getRandomByte() []Bit {
 	byteSlice := make([]byte, 2)
 	rand.Seed(time.Now().Unix())
 	rand.Read(byteSlice)
 	return BytesToBits(byteSlice)
+}
+
+// GetWaterMark takes an email and checks to see if this email
+// is already in the database. If not, it generates a unique
+// watermark and stores it into the database
+func GetWaterMark(email string) []Bit {
+
+	existingWaterMark := sql.GetUserWatermarkFromEmail(email)
+	if existingWaterMark == "" {
+		bits, StringBit := GetRandomBytes()
+		sql.InsertNewUser(email, StringBit)
+		return bits
+	}
+	return ToBitSlice(existingWaterMark)
 }
